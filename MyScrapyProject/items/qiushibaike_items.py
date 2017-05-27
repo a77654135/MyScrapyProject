09d4295ba9c28fb4643ae624e580965031f01db3
talus <talus_wang@sina.com>
2017-05-27 20:59:31 +0800

    糗事百科爬虫，未完成

---
+++ MyScrapyProject/items/qiushibaike_items.py
@@ -0,0 +1,5 @@
+import scrapy
+
+
+class QiushibaikeItem(scrapy.Item):
+    pass
--- MyScrapyProject/spiders/qiushibaike.py
+++ MyScrapyProject/spiders/qiushibaike.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.http.request import Request
 
 
 class QiushibaikeSpider(scrapy.Spider):
     name = 'qiushibaike'
     allowed_domains = ['qiushibaike.com']
-    start_urls = ['http://qiushibaike.com/']
+    start_urls = ['http://www.qiushibaike.com/']
+
+    base_url = r'http://www.qiushibaike.com'
 
     def parse(self, response):
+
+        a_list1 = response.xpath("//div[@id='menu']/a[starts-with(@href,'/')]")
+        for a in a_list1:
+            href = a.xpath('@href').extract_first()
+            menu = a.xpath("text()").extract_first()
+            print u'href:{}   text:{}'.format(href,menu)
+
+
+    # 解析页数
+    def getPageList(self,response):
         pass
